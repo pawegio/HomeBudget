637e504106ac5ec5b5f544bf5ad809eaaabe27ab
Paweł Gajda <pawegio@gmail.com>
2019-08-24 16:37:59 +0200

    Update kotlin to 1.3.50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.41")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.0-RC")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.0-RC2")
