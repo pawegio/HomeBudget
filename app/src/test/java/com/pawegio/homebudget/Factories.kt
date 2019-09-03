f52011abc38e716148337105a583a2ec7a3890ac
Paweł Gajda <pawegio@gmail.com>
2019-09-03 22:21:02 +0200

    Fix factories after domain change

--- app/src/test/java/com/pawegio/homebudget/Factories.kt
+++ app/src/test/java/com/pawegio/homebudget/Factories.kt
@@ -1,10 +1,12 @@
 package com.pawegio.homebudget
 
+import java.math.BigDecimal
+
 fun createMonthlyBudget() =
     MonthlyBudget(
-        plannedIncomes = "10000",
-        plannedExpenses = "10000",
-        actualIncomes = "11000",
-        actualExpenses = "9000",
+        plannedIncomes = BigDecimal(10000),
+        plannedExpenses = BigDecimal(10000),
+        actualIncomes = BigDecimal(11000),
+        actualExpenses = BigDecimal(9000),
         categories = emptyList()
     )
