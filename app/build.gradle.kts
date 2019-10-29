12a8af2741056673a1a3bb8e41f08a6e270f1eab
Paweł Gajda <pawegio@gmail.com>
2019-10-29 22:43:49 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,40 +20,40 @@ android {
             localProperties.load(project.rootProject.file("local.properties").inputStream())
         }
         buildConfigField(
-                "String",
-                "SPREADSHEET_ID",
-                localProperties.getOrDefault("spreadsheetId", "\"\"") as String
+            "String",
+            "SPREADSHEET_ID",
+            localProperties.getOrDefault("spreadsheetId", "\"\"") as String
         )
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
 }
 
-tasks.withType < Test > {
+tasks.withType<Test> {
     @Suppress("UnstableApiUsage")
     useJUnitPlatform()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.2")
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.core:core-ktx:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta2")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta3")
     implementation("androidx.navigation:navigation-fragment-ktx:2.1.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.1.0")
     implementation("org.koin:koin-androidx-viewmodel:2.0.1")
     implementation("org.koin:koin-android-ext:2.0.1")
-    implementation("com.google.android.material:material:1.1.0-beta01")
+    implementation("com.google.android.material:material:1.2.0-alpha01")
     implementation("com.google.android.gms:play-services-auth:17.0.0")
     implementation("com.google.api-client:google-api-client-android:1.23.0") {
         exclude("org.apache.httpcomponents")
@@ -65,8 +65,8 @@ dependencies {
     implementation("com.jakewharton.threetenabp:threetenabp:1.2.1")
     implementation("com.sasank.roundedhorizontalprogress:roundedhorizontalprogress:1.0.1")
 
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.1")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
     testImplementation("com.jraska.livedata:testing-ktx:1.1.0")
     testImplementation("com.github.langara:SMokK:0.0.3")
     testImplementation("org.threeten:threetenbp:1.4.0")
