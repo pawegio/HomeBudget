9e7c5af756484283b5c997d8b658a89623e8189f
Paweł Gajda <pawegio@gmail.com>
2019-11-18 21:06:37 +0100

    Use MockHomeBudgetApi in MainFlowTest

--- app/src/test/java/com/pawegio/homebudget/main/MainFlowTest.kt
+++ app/src/test/java/com/pawegio/homebudget/main/MainFlowTest.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.MutableLiveData
 import com.jraska.livedata.test
 import com.nhaarman.mockitokotlin2.*
 import com.pawegio.homebudget.FlowSpec
-import com.pawegio.homebudget.HomeBudgetApi
 import com.pawegio.homebudget.MonthlyBudget
+import com.pawegio.homebudget.util.MockHomeBudgetApi
 import com.pawegio.homebudget.util.SpreadsheetLauncher
 import com.pawegio.homebudget.util.createMonthlyBudget
+import io.kotlintest.shouldBe
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.consumeAsFlow
@@ -16,16 +17,14 @@ import org.threeten.bp.Clock
 import org.threeten.bp.Instant
 import org.threeten.bp.Month
 import org.threeten.bp.ZoneId
+import kotlin.coroutines.resume
 
 internal class MainFlowTest : FlowSpec({
     "On main flow" - {
         val actions = Channel<MainAction>()
         val monthlyBudget = MutableLiveData<MonthlyBudget>()
         val monthType = MutableLiveData<MonthType>()
-        val loadedMonthlyBudget = createMonthlyBudget()
-        val api = mock<HomeBudgetApi> {
-            onBlocking { getMonthlyBudget(any()) } doReturn loadedMonthlyBudget
-        }
+        val api = MockHomeBudgetApi()
         val spreadsheetLauncher = mock<SpreadsheetLauncher>()
         var clock = Clock.fixed(Instant.parse("2019-04-01T10:15:00.00Z"), ZoneId.systemDefault())
 
@@ -66,37 +65,39 @@ internal class MainFlowTest : FlowSpec({
         }
 
         "get monthly budget for current month" {
-            verifyBlocking(api) { getMonthlyBudget(Month.APRIL) }
+            api.getMonthlyBudget.invocations shouldBe listOf(Month.APRIL)
         }
 
         "on monthly budget loaded with success" - {
+            val loadedMonthlyBudget = createMonthlyBudget()
+            api.getMonthlyBudget.resume(loadedMonthlyBudget)
 
             "update monthly budget" {
                 monthlyBudget.test().assertValue(loadedMonthlyBudget)
             }
-        }
 
-        "on open spreadsheet" - {
-            actions.offer(MainAction.OpenSpreadsheet)
+            "on open spreadsheet" - {
+                actions.offer(MainAction.OpenSpreadsheet)
 
-            "launch spreadsheet" {
-                verify(spreadsheetLauncher).launch()
+                "launch spreadsheet" {
+                    verify(spreadsheetLauncher).launch()
+                }
             }
-        }
 
-        "on select prev month" - {
-            actions.offer(MainAction.SelectPrevMonth)
+            "on select prev month" - {
+                actions.offer(MainAction.SelectPrevMonth)
 
-            "get monthly budget for previous month" {
-                verifyBlocking(api) { getMonthlyBudget(Month.MARCH) }
+                "get monthly budget for previous month" {
+                    api.getMonthlyBudget.invocations.last() shouldBe Month.MARCH
+                }
             }
-        }
 
-        "on select next month" - {
-            actions.offer(MainAction.SelectNextMonth)
+            "on select next month" - {
+                actions.offer(MainAction.SelectNextMonth)
 
-            "get monthly budget for next month" {
-                verifyBlocking(api) { getMonthlyBudget(Month.MAY) }
+                "get monthly budget for next month" {
+                    api.getMonthlyBudget.invocations.last() shouldBe Month.MAY
+                }
             }
         }
     }
