7fb03ac8b3a3072734a6ef33565192686c72a4dd
Paweł Gajda <pawegio@gmail.com>
2020-02-27 22:15:31 +0100

    Update deps and target sdk version 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 apply("../gradle/app-version.gradle")
 
 android {
-    compileSdkVersion(28)
+    compileSdkVersion(29)
     defaultConfig {
         applicationId = "com.pawegio.homebudget"
         minSdkVersion(24)
-        targetSdkVersion(28)
+        targetSdkVersion(29)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
@@ -51,23 +51,23 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.60")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.3")
     implementation("io.reactivex.rxjava2:rxkotlin:2.4.0")
     implementation("com.jakewharton.rxrelay2:rxrelay:2.1.1")
     implementation("android.arch.lifecycle:extensions:1.1.1")
     implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-alpha03")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.1.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.1.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.2.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.2.1")
     implementation("org.koin:koin-androidx-viewmodel:2.0.1")
     implementation("org.koin:koin-android-ext:2.0.1")
-    implementation("com.google.android.material:material:1.2.0-alpha03")
+    implementation("com.google.android.material:material:1.2.0-alpha05")
     implementation("com.google.android.gms:play-services-auth:17.0.0")
     implementation("com.google.api-client:google-api-client-android:1.23.0") {
         exclude("org.apache.httpcomponents")
@@ -85,7 +85,7 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
     testImplementation("com.jraska.livedata:testing-ktx:1.1.1")
-    testImplementation("com.github.langara:SMokK:0.0.3")
+    testImplementation("com.github.langara:SMokK:0.0.4")
     testImplementation("org.threeten:threetenbp:1.4.0")
 }
 
