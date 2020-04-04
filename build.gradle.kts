33b2b3fd95cf8555d8111a93f0f4dc3ec0b39940
Paweł Gajda <pawegio@gmail.com>
2020-04-04 12:01:10 +0200

    Update android build tools to 4.0.0-beta03

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://maven.fabric.io/public")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0-beta01")
+        classpath("com.android.tools.build:gradle:4.0.0-beta03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
         classpath("com.google.gms:google-services:4.3.3")
         classpath("io.fabric.tools:gradle:1.31.2")
