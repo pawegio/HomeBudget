055706ab91cdf589c2ed80ec62f7ae3541d75985
Paweł Gajda <pawegio@gmail.com>
2020-05-20 21:37:23 +0200

    Add about ui

--- app/src/main/java/com/pawegio/homebudget/about/AboutUi.kt
+++ app/src/main/java/com/pawegio/homebudget/about/AboutUi.kt
@@ -1,11 +1,71 @@
 package com.pawegio.homebudget.about
 
 import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
 import android.view.View
-import splitties.views.dsl.core.Ui
-import splitties.views.dsl.core.frameLayout
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.pawegio.homebudget.BuildConfig
+import com.pawegio.homebudget.R
+import com.pawegio.homebudget.util.html
+import splitties.dimensions.dip
+import splitties.resources.txt
+import splitties.views.appcompat.configActionBar
+import splitties.views.appcompat.showHomeAsUp
+import splitties.views.dsl.appcompat.toolbar
+import splitties.views.dsl.core.*
+import splitties.views.dsl.material.appBarLayout
+import splitties.views.dsl.material.defaultLParams
 
 class AboutUi(override val ctx: Context) : Ui {
 
-    override val root: View = frameLayout()
+    override val root: View = verticalLayout {
+        addAppBar()
+        addInfo(txt(R.string.version), BuildConfig.VERSION_NAME)
+        addInfo(txt(R.string.author), txt(R.string.me))
+        addInfo(txt(R.string.app_icon), html(R.string.app_icon_author))
+    }
+
+    private fun LinearLayout.addAppBar() {
+        add(appBarLayout {
+            add(toolbar {
+                title = txt(R.string.about_app)
+                val activity = ctx as? AppCompatActivity ?: return@toolbar
+                activity.setSupportActionBar(this)
+                activity.configActionBar { showHomeAsUp = true }
+            }, defaultLParams())
+        }, lParams(matchParent))
+    }
+
+    private fun LinearLayout.addInfo(label: CharSequence, value: CharSequence) {
+        add(textView {
+            text = label
+            typeface = Typeface.DEFAULT_BOLD
+        }, lParams {
+            topMargin = dip(16)
+            startMargin = dip(48)
+        })
+        add(textView {
+            text = value
+        }, lParams {
+            startMargin = dip(48)
+            bottomMargin = dip(8)
+        })
+    }
+
+}
+
+@Suppress("unused")
+private class AboutUiPreview : FrameLayout {
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr) {
+        addView(AboutUi(context).root)
+    }
 }
--- app/src/main/java/com/pawegio/homebudget/util/ViewDsls.kt
+++ app/src/main/java/com/pawegio/homebudget/util/ViewDsls.kt
@@ -1,10 +1,14 @@
 package com.pawegio.homebudget.util
 
 import android.content.Context
+import android.text.Html
+import android.text.Spanned
 import android.view.View
 import androidx.annotation.IdRes
+import androidx.annotation.StringRes
 import androidx.annotation.StyleRes
 import androidx.cardview.widget.CardView
+import splitties.resources.str
 import splitties.views.dsl.core.NO_THEME
 import splitties.views.dsl.core.Ui
 import splitties.views.dsl.core.view
@@ -39,3 +43,8 @@ inline fun Ui.cardView(
     contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
     return ctx.cardView(id, theme, initView)
 }
+
+// Html
+
+inline fun View.html(@StringRes stringResId: Int): Spanned =
+    Html.fromHtml(str(stringResId), Html.FROM_HTML_MODE_LEGACY)
