7a2faaad183f0ada9e4e230294936e9a80791a43
Paweł Gajda <pawegio@gmail.com>
2020-06-08 22:27:32 +0200

    Update selected date

--- app/src/main/java/com/pawegio/homebudget/main/newexpense/NewExpenseLogic.kt
+++ app/src/main/java/com/pawegio/homebudget/main/newexpense/NewExpenseLogic.kt
@@ -2,18 +2,34 @@
 
 package com.pawegio.homebudget.main.newexpense
 
+import androidx.lifecycle.MutableLiveData
 import com.pawegio.homebudget.HomeBudgetApi
 import io.reactivex.Observable
 import kotlinx.coroutines.rx2.awaitFirst
+import org.threeten.bp.LocalDate
 
 suspend fun NewExpenseLogic(
     actions: Observable<NewExpenseAction>,
+    state: MutableLiveData<NewExpenseState>,
     api: HomeBudgetApi
 ) {
-    actions.awaitFirst()
-    api.addExpense()
+    var selectedDate: LocalDate
+    while (true) {
+        when (val action = actions.awaitFirst()) {
+            is NewExpenseAction.SelectDate -> {
+                selectedDate = action.date
+                state.value = NewExpenseState(selectedDate)
+            }
+            NewExpenseAction.SelectAdd -> api.addExpense()
+        }
+    }
 }
 
+data class NewExpenseState(
+    val selectedDate: LocalDate
+)
+
 sealed class NewExpenseAction {
+    data class SelectDate(val date: LocalDate) : NewExpenseAction()
     object SelectAdd : NewExpenseAction()
 }
--- app/src/test/java/com/pawegio/homebudget/main/newexpense/NewExpenseLogicTest.kt
+++ app/src/test/java/com/pawegio/homebudget/main/newexpense/NewExpenseLogicTest.kt
@@ -1,18 +1,31 @@
 package com.pawegio.homebudget.main.newexpense
 
+import androidx.lifecycle.MutableLiveData
 import com.jakewharton.rxrelay2.PublishRelay
+import com.jraska.livedata.test
 import com.pawegio.homebudget.LogicSpec
 import com.pawegio.homebudget.util.MockHomeBudgetApi
 import io.kotlintest.shouldBe
 import kotlinx.coroutines.launch
+import org.threeten.bp.LocalDate
 
 internal class NewExpenseLogicTest : LogicSpec({
     "On new expense logic" - {
         val actions = PublishRelay.create<NewExpenseAction>()
+        val state = MutableLiveData<NewExpenseState>()
         val api = MockHomeBudgetApi()
 
         launch {
-            NewExpenseLogic(actions, api)
+            NewExpenseLogic(actions, state, api)
+        }
+
+        "on select date" - {
+            val selectedDate = LocalDate.of(2020, 5, 8)
+            actions.accept(NewExpenseAction.SelectDate(selectedDate))
+
+            "update selected date" {
+                state.test().assertValue { it.selectedDate == selectedDate }
+            }
         }
 
         "on select add" - {
