6118c0836b64391a023acc1f00bb9c9f7b8d4599
Paweł Gajda <pawegio@gmail.com>
2020-08-04 20:10:02 +0200

    Update android build tools to 4.1.0-beta05

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-beta04")
+        classpath("com.android.tools.build:gradle:4.1.0-beta05")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
         classpath("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.0")
