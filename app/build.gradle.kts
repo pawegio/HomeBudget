7f4dd0aea33759d1e4f9ca4c96b342c03d48e348
Paweł Gajda <pawegio@gmail.com>
2020-10-17 16:41:33 +0200

    Update kotlin to 1.4.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.4")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.0")
     }
