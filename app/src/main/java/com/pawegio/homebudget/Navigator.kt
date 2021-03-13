3b8296613ab344c5da4e5a0844e688d2dd857f2a
Paweł Gajda <pawegio@gmail.com>
2021-03-13 23:39:00 +0100

    Remove redundant pending actions management from navigator

--- app/src/main/java/com/pawegio/homebudget/Navigator.kt
+++ app/src/main/java/com/pawegio/homebudget/Navigator.kt
@@ -3,10 +3,7 @@
 package com.pawegio.homebudget
 
 import android.app.Application
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
-import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.navigation.NavController
 
 interface Navigator {
@@ -19,41 +16,11 @@ class NavigatorImpl(
     private val getNavController: () -> NavController?
 ) : Navigator, LifecycleObserver {
 
-    private val pendingActions = mutableListOf<() -> Any?>()
-
-    private var isAppInForeground = false
-
-    init {
-        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onAppInForeground() {
-        isAppInForeground = true
-        pendingActions.run {
-            forEach { it.invoke() }
-            clear()
-        }
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    fun onAppInBackground() {
-        isAppInForeground = false
-    }
-
-    override fun navigate(destinationId: Int) = tryToInvoke {
+    override fun navigate(destinationId: Int) {
         getNavController()?.run { navigate(destinationId) }
     }
 
-    override fun popBackStack() = tryToInvoke {
+    override fun popBackStack() {
         getNavController()?.run { popBackStack() }
-    }
-
-    private fun tryToInvoke(action: () -> Any?) {
-        if (isAppInForeground) {
-            action.invoke()
-        } else {
-            pendingActions.add(action)
-        }
     }
 }
