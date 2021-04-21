af0b258fc0fdeea0ff90c18469f27f89699023ac
Paweł Gajda <pawegio@gmail.com>
2021-04-21 21:09:22 +0200

    Update android gradle plugin to 4.2.0-rc01

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-beta06")
+        classpath("com.android.tools.build:gradle:4.2.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
         classpath("com.google.gms:google-services:4.3.4")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
