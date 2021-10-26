0910dccdf7cfe8f3be4de0676979a8b5cad7cb62
Paweł Gajda <pawegio@gmail.com>
2021-10-26 22:42:14 +0200

    Update android gradle plugin to 7.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
