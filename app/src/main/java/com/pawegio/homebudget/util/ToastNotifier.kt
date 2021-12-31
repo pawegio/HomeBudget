dd85cd0f4051e1f290325de19ab9306381007eda
Paweł Gajda <pawegio@gmail.com>
2021-12-31 15:11:51 +0100

    Update ToastNotifier to support format args

--- app/src/main/java/com/pawegio/homebudget/util/ToastNotifier.kt
+++ app/src/main/java/com/pawegio/homebudget/util/ToastNotifier.kt
@@ -5,11 +5,20 @@ import android.widget.Toast
 
 interface ToastNotifier {
     fun notify(textResId: Int)
+    fun notify(textResId: Int, vararg args: Any)
 }
 
 class ToastNotifierImpl(private val context: Context) : ToastNotifier {
 
+    private var toast: Toast? = null
+
     override fun notify(textResId: Int) {
-        Toast.makeText(context, textResId, Toast.LENGTH_SHORT).show()
+        toast?.cancel()
+        toast = Toast.makeText(context, textResId, Toast.LENGTH_SHORT).apply { show() }
+    }
+
+    override fun notify(textResId: Int, vararg args: Any) {
+        toast?.cancel()
+        toast = Toast.makeText(context, context.getString(textResId, args), Toast.LENGTH_SHORT).apply { show() }
     }
 }
