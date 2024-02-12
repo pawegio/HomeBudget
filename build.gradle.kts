2d5cfc18c07e072508c5c691905c7d39070e90e8
Paweł Gajda <pawegio@gmail.com>
2024-02-12 22:59:54 +0100

    Upgrade project to AGP 8.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.1")
-        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.google.gms:google-services:4.4.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
